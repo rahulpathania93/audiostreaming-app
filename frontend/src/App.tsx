3b348e0438d20e1457cdd3130c89f65095d6734f
Rahul Pathania <rahul.pathania093@gmail.com>
2025-09-22 18:07:03 +0530

    setting up front and backend connection

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {console.log("App loaded--app.tsx");
+  return ( // Added parentheses for clarity, though optional with fragments
+    <>
+      <h1>Hello World</h1>
+      <p>This is my first React app 🚀</p>
+    </>
+  );
+}
+
+export default App;
